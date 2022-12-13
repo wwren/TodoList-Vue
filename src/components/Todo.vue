2c385383ad41c763cfcfc202b9b6e63ec2a84e6c
Ran Ding <ran@Rans-MBP.hub>
2022-12-13 12:04:43 +1100

    Add todo list

---
+++ src/components/Todo.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-item">
+      <input
+        type="checkbox"
+        :id="todo.id"
+        :name="todo.id"
+        :checked="todo.done"
+        @change="$emit('toggleDone', todo.id)"
+      />
+      <p :class="{ done: todo.done }">{{ todo.task }}</p>
+    </div>
+    <div class="todo-tool">
+      <i class="fa-solid fa-pen" @click="$emit('editTodo', todo.id)"></i>
+      <i class="fa-solid fa-xmark" @click="$emit('deleteTodo', todo.id)"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  // data() {
+  //   return {
+  //     check: false,
+  //   };
+  // },
+  props: {
+    todo: Object,
+    // checked: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+.todo-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+input:hover {
+  cursor: pointer;
+}
+.todo-item {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  gap: 10px;
+}
+
+.todo-tool {
+  display: flex;
+  justify-content: space-around;
+  gap: 10px;
+}
+.todo-tool i {
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul>
+    <li v-for="todo of todos" v-bind:key="todo.id">
+      <Todo
+        :todo="todo"
+        @toggleDone="$emit('toggleDone', todo.id)"
+        @deleteTodo="$emit('deleteTodo', todo.id)"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+import Todo from "./Todo.vue";
+
+export default {
+  name: "Todos",
+  props: {
+    todos: Array,
+  },
+  components: {
+    Todo,
+  },
+  emits: ["toggleDone"],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0 20px;
+}
+
+ul li {
+  border-top: 0.5px solid grey;
+}
+
+ul li:last-child {
+  border-bottom: 0.5px solid grey;
+}
+</style>
